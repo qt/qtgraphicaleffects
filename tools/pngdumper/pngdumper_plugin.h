bd65b41e35cd3c8fdde13f1a47735b30b5214911
Lars Knoll <lars.knoll@nokia.com>
2012-05-27 12:00:49 +0200

    Port pngdumper to the new plugin format
    Change-Id: I48d6b716f309323e1df35a2d0d6e5d914fcf0f54

--- tools/pngdumper/pngdumper_plugin.h
+++ tools/pngdumper/pngdumper_plugin.h
@@ -47,7 +47,8 @@
 class ItemcapturerPlugin : public QQmlExtensionPlugin
 {
     Q_OBJECT
-    
+    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QQmlExtensionInterface")
+
 public:
     void registerTypes(const char *uri);
 };
