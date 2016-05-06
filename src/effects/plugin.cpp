5d286546f0041dc1afe51c9a202e4d34fef69c38
J-P Nurmi <jpnurmi@qt.io>
2016-05-06 15:36:42 +0200

    Use the pre-defined QQmlExtensionInterface_iid macro
    The others were changed in 5.6 commit d01a936. This is the last
    remaining occurrence in 5.7.
    
    Task-number: QTBUG-53208
    Change-Id: I9d8d3f8a4af590fd1b4d4883ae559119cda3d7d0
    Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>

--- src/effects/plugin.cpp
+++ src/effects/plugin.cpp
@@ -53,7 +53,7 @@ QT_BEGIN_NAMESPACE
 class QtGraphicalEffectsPlugin : public QQmlExtensionPlugin
 {
     Q_OBJECT
-    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QQmlExtensionInterface")
+    Q_PLUGIN_METADATA(IID QQmlExtensionInterface_iid)
 
 public:
     QtGraphicalEffectsPlugin(QObject *parent = 0) : QQmlExtensionPlugin(parent) { initResources(); }
